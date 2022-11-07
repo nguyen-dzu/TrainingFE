ed5f73e1da889371c59f05e912dc09bb8e64a77e
Nguyễn Hoàng Vũ <81034816+nguyen-dzu@users.noreply.github.com>
2022-11-07 23:43:00 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from '~/Components/Header/Header';
+import Banner from '~/Components/Banner/Banner';
+import BestProduct from './Components/Bestproduct/BestProduct';
+import Product from './Components/Product/Product';
+import BrandStory from './Components/BrandStory/BrandStory';
+import HappenedIssue from './Components/Happened/HappenedIssue';
+import WhatHappened from './Components/WhatHappened/WhatHappened';
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
